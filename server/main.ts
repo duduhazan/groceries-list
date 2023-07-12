a9f29acb08d9327c94b98e630b64ec23d9b4d547
duduhazan <duduhazan123@gmail.com>
2023-07-12 10:36:48 +0300

    removing console.log, bug fixed

--- server/main.ts
+++ server/main.ts
@@ -10,8 +10,6 @@ import { writeFile } from "fs/promises";
 
 async function startServer() {
   await connectDB(process.env.DB_URL!);
-  console.log(process.env.CLIENT_URL);
-  
 
   const port = process.env.PORT;
   const app = express();
